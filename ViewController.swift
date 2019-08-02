219818c525424d2282d90ffb05971eb8208b8770
Mikolaj Lukasik <mlukasik@gmail.com>
2019-08-02 22:11:55 +0100

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Mikolaj Lukasik on 02/08/2019.
+//  Copyright © 2019 Mikolaj Lukasik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
